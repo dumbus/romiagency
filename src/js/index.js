2713ba2c4b325425e70f021727e05a062729e29a
dumbus <dumbusx@gmail.com>
2022-06-13 18:39:39 +0300

    add cases and footer

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,11 @@ import '../sass/style.scss';
 import openModal from './modules/openModal';
 import manageModal from './modules/manageModal';
 import formManage from './modules/formManage';
+import manageSwiper from './modules/manageSwiper'
 
 window.addEventListener('DOMContentLoaded', async () => {
   openModal();
   manageModal();
   await formManage();
+  manageSwiper();
 });
--- src/js/modules/openModal.js
+++ src/js/modules/openModal.js
@@ -3,18 +3,20 @@ export const openModal = async () => {
   const mainHomeBlock = document.querySelector('.main-home');
   const modalBlock = document.querySelector('.modal');
   const homeBtn = document.querySelector('.header-button');
+  const header = document.querySelector('.header');
 
   modalOpenBtns.forEach(btn => {
     btn.addEventListener('click', () => {
       modalBlock.classList.remove('hidden');
       mainHomeBlock.classList.add('hidden');
-      console.log('opened');
+      header.classList.add('header-dark');
     });
   });
 
   homeBtn.addEventListener('click', () => {
     modalBlock.classList.add('hidden');
     mainHomeBlock.classList.remove('hidden');
+    header.classList.remove('header-dark');
   });
 };
 

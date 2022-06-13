042d84b20872a532c071a6ab1e02a480ccf78be4
dumbus <dumbusx@gmail.com>
2022-06-13 19:09:37 +0300

    add clients section

--- src/js/modules/manageSwiper.js
+++ src/js/modules/manageSwiper.js
@@ -1,5 +1,12 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
 export const manageSwiper = () => {
-  
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    spaceBetween: 50,
+    slidesPerView: 3
+  });
 };
 
 export default manageSwiper;
